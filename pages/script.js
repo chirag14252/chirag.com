c8df4b3bf0aa2d3c5df34961b52310de6e1c05a2
chirag142452 <chi.chiragbhardwaj@gmail.com>
2023-08-29 13:09:55 +0530

    changes

---
+++ pages/script.js
@@ -0,0 +1,53 @@
+// Assuming you have an HTML form with an ID 'myForm' and a submit button with an ID 'submitButton'
+document.getElementById('myForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    
+    // Gather form data
+    var formData = new FormData(event.target);
+    
+    // Convert form data to an object
+    var formDataObject = {};
+    formData.forEach(function (value, key) {
+      formDataObject[key] = value;
+    });
+  
+    // Send the form data to your Google Apps Script
+    submitForm(formDataObject);
+  });
+  
+  function submitForm(formData) {
+    // Create a new XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+  
+    // Set up the request
+    xhr.open('POST', 'https://your-apps-script-url', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+  
+    // Define the data to be sent
+    var data = JSON.stringify(formData);
+  
+    // Define the function to handle the response
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        var response = JSON.parse(xhr.responseText);
+        if (response.result === 'success') {
+          // Redirect to the URL provided by the response
+          window.location.href = response.redirect;
+        } else {
+          // Handle error
+          console.error('Error:', response.error);
+        }
+      } else {
+        console.error('Request failed with status:', xhr.status);
+      }
+    };
+  
+    // Handle network errors
+    xhr.onerror = function () {
+      console.error('Network error occurred');
+    };
+  
+    // Send the request
+    xhr.send(data);
+  }
+  
